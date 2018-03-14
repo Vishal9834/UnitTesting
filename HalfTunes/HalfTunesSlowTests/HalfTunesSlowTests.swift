8449e5615e6c51aa57de0af56eb8c9a5e703b6cd
Vishal9834 <bodkevishal22@gmail.com>
2018-03-14 13:05:22 +0530

    First commit

---
+++ HalfTunes/HalfTunesSlowTests/HalfTunesSlowTests.swift
@@ -0,0 +1,84 @@
+//
+//  HalfTunesSlowTests.swift
+//  HalfTunesSlowTests
+//
+//  Created by Vishal Bodke on 12/03/18.
+//  Copyright © 2018 Ray Wenderlich. All rights reserved.
+//
+
+import XCTest
+@testable import HalfTunes
+
+class HalfTunesSlowTests: XCTestCase {
+    
+    var sessionUnderTest: URLSession!
+
+    override func setUp() {
+        super.setUp()
+        sessionUnderTest = URLSession(configuration: URLSessionConfiguration.default)
+
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sessionUnderTest = nil
+        super.tearDown()
+    }
+    
+    // Asynchronous test: success fast, failure slow
+    func testValidCallToiTunesGetsHTTPStatusCode200() {
+        // given
+        let url = URL(string: "https://itunes.apple.com/search?media=music&entity=song&term=abba")
+        // 1
+        let promise = expectation(description: "Status code: 200")
+        
+        // when
+        let dataTask = sessionUnderTest.dataTask(with: url!) { data, response, error in
+            // then
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+                return
+            } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if statusCode == 200 {
+                    // 2
+                    promise.fulfill()
+                } else {
+                    XCTFail("Status code: \(statusCode)")
+                }
+            }
+        }
+        dataTask.resume()
+        // 3
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    
+    // Asynchronous test: faster fail
+    func testCallToiTunesCompletes() {
+        // given
+        let url = URL(string: "https://itunes.apple.com/search?media=music&entity=song&term=abba")
+        // 1
+        let promise = expectation(description: "Completion handler invoked")
+        var statusCode: Int?
+        var responseError: Error?
+        
+        // when
+        let dataTask = sessionUnderTest.dataTask(with: url!) { data, response, error in
+            statusCode = (response as? HTTPURLResponse)?.statusCode
+            responseError = error
+            // 2
+            promise.fulfill()
+        }
+        dataTask.resume()
+        // 3
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        // then
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 200)
+    }
+    
+    
+    
+}
